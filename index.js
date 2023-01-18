55b42cd96df1f8db0443ec87e2eefac954d4ec5f
DaleStewart <dalestewart@ibm.com>
2023-01-18 14:52:53 -0500

    Added body-parser, and a post method using the same endpoint just to make sure I could There's now enough pieces in place for me to start working on the story

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
+//port setup
 const PORT = 8080;
 
+//app setup
 const app = express();
-app.get("/url", (req, res, next) => {
+app.use(bodyParser.json());
+
+
+app.get("/url", (req, res) => {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
-app.listen(8080, () => {
+
+app.post('/url', function (req, res) {
+    var body = req.body;
+    //res = body;
+    const somee = {msg:'success', status:'okay'}
+    res.send(somee);
+  });
+
+app.listen(PORT, () => {
     console.log("Server running on port 8080");
 });
